019378238a4f027759b06d63834bada79b5a9b95
yys-4 <61109623+yys-4@users.noreply.github.com>
2026-01-14 10:25:43 +0700

    Update next.config.ts for export output and basePath
    Set output to 'export' and add basePath for portfolio.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/portofolio-8bit',  // Add this line
   reactCompiler: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
